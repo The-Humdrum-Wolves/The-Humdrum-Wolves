b7494cb4a2440015f303bc3b74bf2a9a70e3bc50
Xenoleth <petar.yerzhabek@abv.bg>
2017-08-18 15:25:35 +0300

    added navbar and footer skeleton

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,22 +7,26 @@ import { HomeComponent } from './components/home/home.component';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
 import { PageNotFoundComponent } from './components/pageNotFound/pageNotFound.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
-const appRoutes: Routes = [
-  { path: '', component: HomeComponent },
+const appRoutes: Routes = [  
   { path: 'home', component: HomeComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavbarComponent,
+    FooterComponent,
     HomeComponent,
     RegisterComponent,
     LoginComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
