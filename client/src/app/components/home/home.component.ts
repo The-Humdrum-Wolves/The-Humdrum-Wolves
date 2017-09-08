99d49b09a7a17af093c77a5975cf3b85775a54a4
Petar Bochukov <petar.bochukov@abv.bg>
2017-09-08 17:54:06 +0300

    Add home component

---
+++ client/src/app/components/home/home.component.ts
@@ -0,0 +1,27 @@
+import { ChampionshipRankingsService } from './home.service';
+import { IChampionshipRankings } from './home.component.module';
+import { Component, OnInit, Inject } from '@angular/core';
+
+@Component({
+    selector: 'hw-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
+    // test
+    // kakbvos ta
+    // EXT_texture_filter_anisotropic
+})
+
+export class HomeComponent implements OnInit {
+    public championshipRankings: IChampionshipRankings[];
+
+    constructor(private championshipRankingsService: ChampionshipRankingsService) {}
+   // constructor(@Inject(ChampionshipRankingsService) private championshipRankingsService: ChampionshipRankingsService) {}
+
+    ngOnInit() {
+        this.championshipRankingsService.getRankings()
+        .subscribe(info => {
+            this.championshipRankings = info;
+            console.log(info);
+        });
+    }
+}
