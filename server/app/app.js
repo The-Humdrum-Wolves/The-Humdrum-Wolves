d8dea0ab278a75af0f1f3c72d6af570d008d7903
Xenoleth <petar.yerzhabek@abv.bg>
2017-09-10 08:12:36 +0300

    Add json parse to requests

--- server/app/app.js
+++ server/app/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const init = (data) => {
     const app = express();
 
-    // TODO: Config application
+    // Config application
+    app.use(bodyParser.json());
     require('../config/auth.config')(app, data);
 
     // add routers
--- server/app/routers/users.router.js
+++ server/app/routers/users.router.js
@@ -2,6 +2,10 @@ const attachTo = (app, data) => {
     app.get('/', (req, res) => {
         res.send('ufff');
     });
+
+    app.post('/users', (req, res) => {
+        console.log(req.body);
+    });
 };
 
 module.exports = { attachTo };
