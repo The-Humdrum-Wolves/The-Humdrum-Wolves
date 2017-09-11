a05f5c56cd0585f2e1f2037be9c2fc1a997aecf1
Martina_Shebova <martina2314@gmail.com>
2017-09-11 22:52:03 +0300

    More changes for Heroku

--- server/app/app.js
+++ server/app/app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const path = require('path');
 
 const init = (data) => {
     const app = express();
@@ -10,17 +9,11 @@ const init = (data) => {
     app.use(cors());
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
-
-    app.use(express.static(__dirname + '/dist'));
     
     require('../config/auth.config')(app, data);
 
     // add routers
     require('./routers').attachTo(app, data);
-
-    app.get('/*', (req, res) => {
-        res.sendFile(path.join(__dirname + '/dist/index.html'));
-    });
 
     return Promise.resolve(app);
 };
